cb542dc846ef285ed3ac1f49d0cee22c4e6e38e9
Angus <anguscheng389@gmail.com>
2021-06-10 22:57:56 +1000

    Added randomColor and grid size creation functionality to script.js

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 /** Create 16 x 16 grid of squares */
 const container = document.querySelector('.container');
-    for (i = 0; i < (16 * 16); i++) {
+createSquareGrid(16);
+
+function randomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random()*16)];
+    }
+
+    return color;
+}
+
+/** Color squares */
+function colorSquare(e) {
+    e.currentTarget.style.backgroundColor = randomColor();
+}
+
+function createSquareGrid(size) {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    container.style.gridTemplateRows = `repeat(${size}, auto)`;
+    container.style.gridTemplateColumns = `repeat(${size}, auto)`;
+
+    for (i = 0; i < (size * size); i++) {
         const divTag = document.createElement('div');
         divTag.classList.add('square');
         container.appendChild(divTag);
     }
 
-/** Color squares */
-function colorSquare(e) {
-    e.currentTarget.style.backgroundColor = 'red';
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => 
+        square.addEventListener('mouseover', colorSquare));
 }
 
-/** Event for coloring squares */
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => 
-    square.addEventListener('mouseover', colorSquare));
+function handleClearButton() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.style.backgroundColor = 'black');
+    let gridSize = 
+        window.prompt('How many squares per side would you like to make your grid? (max 50)');
+    if (gridSize > 50) {
+        window.alert('Please enter a value below 50');
+        handleClearButton();
+    }
+    if (!(/^[0-9.,]+$/.test(gridSize))) {
+        window.alert('Please enter an integer value');
+        handleClearButton();
+    }
+
+    createSquareGrid(gridSize);
+}
+
+const clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener('click', handleClearButton);
